008ba0f125a882fd0e6b38bc1d5053159e553551
KingCrimsonN <MSUA1307@gmail.com>
2023-02-03 22:16:14 +0200

    Added test obstacles, changed the movement a bit

--- Assets/SalmonScript.cs
+++ Assets/SalmonScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SalmonScript : MonoBehaviour
 {
+
   // Start is called before the first frame update
   void Start()
   {
@@ -14,5 +15,6 @@ public class SalmonScript : MonoBehaviour
   void Update()
   {
     transform.rotation = transform.parent.rotation;
+    transform.position = Vector3.MoveTowards(transform.position, transform.parent.position, 2 * Time.deltaTime);
   }
 }
