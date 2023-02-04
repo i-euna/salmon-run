de82ed184762d2182e1e8146fcbcd74347f654df
KingCrimsonN <MSUA1307@gmail.com>
2023-02-04 10:36:02 +0200

    Tweaked the movement

--- Assets/Scripts/FishCreator.cs
+++ Assets/Scripts/FishCreator.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class FishCreator : MonoBehaviour
 {
 
-    public GameObject fish;
-    public List<GameObject> fishes;
-    // Start is called before the first frame update
-    void Start()
-    {
-        for(int i = 0; i < 3; i++)
-        {
-            GameObject newFish = Instantiate(fish, fish.transform.position, fish.transform.rotation);
-            fishes.Add(newFish);
-        }
-    }
+  public GameObject fish;
+  public List<GameObject> fishes;
 
-    // Update is called once per frame
-    void Update()
+  public int fish_amount;
+  // Start is called before the first frame update
+  void Start()
+  {
+    for (int i = 0; i < fish_amount; i++)
     {
-        
+      GameObject newFish = Instantiate(fish, fish.transform.position, fish.transform.rotation);
+      fishes.Add(newFish);
     }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
 }
--- Assets/Scripts/FlockManager.cs
+++ Assets/Scripts/FlockManager.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 
 public class FlockManager : MonoBehaviour
 {
-    private Vector2 mousePos;
-    private Vector2 myPosition;
-    int speed = 2;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //float angle = Random.Range(0, 2 * Mathf.PI);
-        //transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-    }
+  private Vector2 mousePos;
+  private Vector2 myPosition;
 
-    // Update is called once per frame
-    void Update()
-    {
-        mousePos = getPosition();
-        myPosition = transform.position;
-        float angle = Mathf.Atan2(myPosition.y - mousePos.y, myPosition.x - mousePos.x) * Mathf.Rad2Deg;
-        transform.position = Vector2.MoveTowards(transform.position, mousePos, speed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 90));
-    }
+  private Vector2 movePosition = new Vector2(0f, 0f);
 
-    Vector2 getPosition()
-    {
-        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    }
+  public Rigidbody2D rb;
+  float speed = 2f;
+  // Start is called before the first frame update
+  void Start()
+  {
+    //float angle = Random.Range(0, 2 * Mathf.PI);
+    //transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    mousePos = getPosition();
+    myPosition = transform.position;
+    movePosition = Vector2.Lerp(myPosition, mousePos, speed * 0.01f);
+    float angle = Mathf.Atan2(myPosition.y - mousePos.y, myPosition.x - mousePos.x) * Mathf.Rad2Deg;
+    // transform.position = Vector2.MoveTowards(transform.position, mousePos, speed * Time.deltaTime);
+    transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 90));
+  }
+
+  private void FixedUpdate()
+  {
+    rb.MovePosition(movePosition);
+  }
+
+  Vector2 getPosition()
+  {
+    return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+  }
 }
