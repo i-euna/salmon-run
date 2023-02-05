46bf6b0b9ba034db15adc161b300b02e18fe90a1
KingCrimsonN <MSUA1307@gmail.com>
2023-02-05 16:35:55 +0200

    Did some stuff

--- Assets/Scripts/Menus/GameOverManager.cs
+++ Assets/Scripts/Menus/GameOverManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameOverManager : MonoBehaviour
 {
@@ -8,16 +10,18 @@ public class GameOverManager : MonoBehaviour
   public GameObject GameOverPanel;
   public Animator animator;
   public bool isGameOver = false;
+  public TMP_Text signText;
   private void Start()
   {
     instance = this;
   }
   private void OnCollisionEnter2D(Collision2D collision)
   {
+    signText.text = FishManager.fish_amount.ToString() + " Salmons Survived";
     GameOverPanel.SetActive(true);
     animator.SetBool("Win", true);
-
     isGameOver = true;
+
     Invoke("Pause", 5f);
   }
 
