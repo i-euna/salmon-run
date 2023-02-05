2d300e70e85ff690a9ffcbb5f3ea42726ad8066f
KingCrimsonN <MSUA1307@gmail.com>
2023-02-05 09:47:21 +0200

    Added Euna's changes to sound triggers, refining the menu scene

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void QuitGame()
+  {
+    Debug.Log("Quitting the game");
+    Application.Quit();
+  }
+}
