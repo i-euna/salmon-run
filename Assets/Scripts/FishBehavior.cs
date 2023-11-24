45e9a8e5cc4782bc362514a9cfca5b6e8233ff43
Euna Islam <euna.islam@gmail.com>
2023-11-24 16:59:45 +0200

    Updated fish behavior

--- Assets/Scripts/FishBehavior.cs
+++ Assets/Scripts/FishBehavior.cs
@@ -15,11 +15,17 @@ public class FishBehavior : MonoBehaviour
         zPos = transform.position.z;
     }
 
-    public void MoveTowardsTarget()
+    public void MoveAndRotateTowardsTarget()
     {
         Vector3 targetPosition = new Vector3(TargetPosition.Value.x, 
                                             TargetPosition.Value.y,
                                             zPos);
+        //move towards target
         transform.position = Vector3.Lerp(transform.position, targetPosition, MovementSpeed.Value * Time.deltaTime);
+
+        //rotate towards target
+        Vector3 direction = targetPosition - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 }
